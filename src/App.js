7a90747dedf78a32b15bede725406a05d45a106e
Harrison-code <harrisondmix@gmail.com>
2019-09-14 17:07:49 +0800

    Update project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from '@apollo/react-hooks';
+
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import Login from './pages/Login'
+import Home from './pages/Home'
+import Register from './pages/Register'
 
 
 const client = new ApolloClient({
   uri: 'https://48p1r2roz4.sse.codesandbox.io',
 });
+
 const App = () => (
-  <ApolloProvider client={client}>
-    <Login />
-  </ApolloProvider>
+  <Router>
+    <Route exact path="/" component={Home}/>
+    <Route exact path="/login" component={Login}/>
+    <Route exact path="/register" component={Register}/>
+  </Router>
 );
 
 export default App
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,7 +5,7 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
+
 import Paper from '@material-ui/core/Paper';
 import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
@@ -13,17 +13,19 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 
+import { Link } from 'react-router-dom'
+
 function Copyright() {
     return (
         <Typography variant="body2" color="textSecondary" align="center">
             {'Copyright © '}
             <Link color="inherit" href="https://material-ui.com/">
-                Your Website
+                RTC TESDA
       </Link>{' '}
             {new Date().getFullYear()}
             {'. Built with '}
             <Link color="inherit" href="https://material-ui.com/">
-                Material-UI.
+                CMD.
       </Link>
         </Typography>
     );
@@ -100,23 +102,23 @@ export default function Login() {
                             control={<Checkbox value="remember" color="primary" />}
                             label="Remember me"
                         />
+
                         <Button
                             type="submit"
                             fullWidth
                             variant="contained"
                             color="primary"
                             className={classes.submit}
+                            to="/"
                         >
-                            Sign In
-            </Button>
+                            <Link to="/" >
+                                    {"Sign in"}
+                                </Link>
+                        </Button>
                         <Grid container>
-                            <Grid item xs>
-                                <Link href="#" variant="body2">
-                                    Forgot password?
-                </Link>
-                            </Grid>
+                         
                             <Grid item>
-                                <Link href="http://localhost:3000/Signup" variant="body2">
+                                <Link to="/register" variant="body2">
                                     {"Don't have an account? Sign Up"}
                                 </Link>
                             </Grid>
